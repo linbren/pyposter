2f6dbfbaa72a06fdf999146c3392702dc56eb18a
ChrisLeeGit <christopherlee199398@gmail.com>
2016-07-17 01:14:47 +0800

    GUI 版本基本功能实现

--- src/pyposter_gui.py
+++ src/pyposter_gui.py
@@ -11,9 +11,9 @@
 import os
 import sys
 from gettext import gettext as _
-from tkinter import Frame, LabelFrame, OptionMenu, Listbox, Button, Entry, Label, Tk, StringVar
+from tkinter import Frame, LabelFrame, OptionMenu, Listbox, Scrollbar, Button, Entry, Label, Tk, StringVar
 from tkinter.constants import *
-from tkinter.messagebox import showinfo
+from tkinter.messagebox import showinfo, showerror
 from tkinter.filedialog import askdirectory
 from utils import config_logger
 from pyposter import PyPoster
@@ -30,20 +30,21 @@ class MainWindow(Frame):
     def __init__(self, master=None, cnf={}, **kwargs):
         super(MainWindow, self).__init__(master, cnf, **kwargs)
         self._tags = StringVar(self)
-        self._category = StringVar(self)
         self._post_title = StringVar(self)
         self._rpc_addr = StringVar(self)
         self._port = StringVar(self)
         self._username = StringVar(self)
         self._password = StringVar(self)
         self._operation = StringVar(self)
-        self._blog_dir = None
+        self._blog_path = StringVar(self)
+        self._pyposter = None
+        self._categories = None
         self.make_widgets()
         self.pack(fill=BOTH, expand=YES, padx=10, pady=10)
         self.center_window()
 
     def make_widgets(self):
-        self.make_post_title_frame()
+        self.make_post_info_frame()
         self.make_server_frame()
         # self.make_operations_frame()
         self.make_options_frame()
@@ -60,8 +61,16 @@ class MainWindow(Frame):
         Entry(options_frm, textvariable=self._tags, font=FONT_DEFAULT, width=30).grid(row=0, column=1, sticky=E, padx=5)
         # 文章分类
         Label(options_frm, text='选择文章分类：', font=FONT_DEFAULT).grid(row=1, column=0, sticky=W, padx=5)
-        Button(options_frm, text='更新分类', font=FONT_DEFAULT).grid(row=1, column=1, sticky=E, padx=5)
-        Listbox(options_frm, font=FONT_DEFAULT, width=46).grid(row=2, column=0, columnspan=2, sticky=W, padx=5, pady=2)
+        Button(options_frm, text='更新分类', font=FONT_DEFAULT, command=self._update_category).grid(row=1, column=1, sticky=E, padx=5)
+
+        cat_frm = Frame(options_frm)
+        cat_frm.grid(row=2, column=0, columnspan=2, sticky=W, padx=5, pady=2)
+
+        vs = Scrollbar(cat_frm)
+        vs.pack(side=RIGHT, fill=Y)
+        self._categories = Listbox(cat_frm, width=44, font=FONT_DEFAULT, yscrollcommand=vs.set)
+        vs.config(command=self._categories.yview)
+        self._categories.pack(side=LEFT, fill=BOTH, expand=YES)
 
     def make_operations_frame(self):
         # 发布操作
@@ -79,23 +88,32 @@ class MainWindow(Frame):
         server_frame = LabelFrame(self, text='服务器信息', font=FONT_DEFAULT)
         server_frame.pack(fill=X, pady=5)
         # 一次创建完
-        rows = [('RPC 地址：', self._rpc_addr),
-                ('端口：', self._port),
-                ('用户名：', self._username),
-                ('密码：', self._password)]
+        rows = [('RPC 地址：', self._rpc_addr, None),
+                # ('端口：', self._port, None),
+                ('用户名：', self._username, None),
+                ('密码：', self._password, '*')]
         for index, row in enumerate(rows):
             Label(server_frame, text=row[0], font=FONT_DEFAULT).grid(row=index, column=0, sticky=W, padx=5, pady=2)
-            Entry(server_frame, textvariable=row[1], font=FONT_DEFAULT, width=34).grid(row=index, column=1, sticky=E,
-                                                                                       padx=5, pady=2)
+            Entry(server_frame, textvariable=row[1], show=row[2], font=FONT_DEFAULT, width=34).grid(row=index,
+                                                                                                    column=1, sticky=E, padx=5, pady=2)
 
-    def make_post_title_frame(self):
+    def make_post_info_frame(self):
         # 文章标题
-        title_frm = Frame(self)
-        title_frm.pack(side=TOP, fill=X, pady=5)
-        Label(title_frm, text='文章标题：', font=FONT_DEFAULT).pack(side=LEFT)
-        Entry(title_frm, textvariable=self._post_title, font=FONT_DEFAULT).pack(side=RIGHT, fill=BOTH, expand=YES)
+        info_frm = LabelFrame(self, text='博客信息', font=FONT_DEFAULT)
+        info_frm.pack(side=TOP, fill=X, pady=5)
 
-    def center_window(self, width=450, height=500):
+        rows = [
+            ('博客目录：', self._blog_path),
+            ('博客标题：', self._post_title)
+        ]
+
+        for index, row in enumerate(rows):
+            Label(info_frm, text=row[0], font=FONT_DEFAULT).grid(row=index, column=0, sticky=W,
+                                                                 padx=5, pady=2)
+            Entry(info_frm, textvariable=row[1], font=FONT_DEFAULT, width=35).grid(row=index, column=1, sticky=E,
+                                                                                   padx=5, pady=2)
+
+    def center_window(self, width=450, height=520):
         screen_width = self.master.winfo_screenwidth()
         screen_height = self.master.winfo_screenheight()
         x = (screen_width / 2) - (width / 2)
@@ -103,19 +121,57 @@ class MainWindow(Frame):
         self.master.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
     def _open_dir(self):
-        self._blog_dir = askdirectory()
+        blog_path = askdirectory()
 
         # 获取目录下的博客标题等信息
-        pass
+        blog_file = [x for x in os.listdir(blog_path)
+                     if x != 'post.conf' and
+                     not os.path.isdir(os.path.join(blog_path, x))]
+
+        if len(blog_file) == 1:
+            self._post_title.set(os.path.splitext(blog_file[0])[0])
+            self._blog_path.set(blog_path)
+        else:
+            logging.error('Invalid blog path')
+            showerror('提示', '无效的博客目录！')
 
     def _confirm(self):
-        pass
+        if not self._pyposter:
+            self._init_pyposter()
+
+        if self._pyposter:
+            self._pyposter.post(self._post_title.get(),
+                                self._categories.get('active'),
+                                self._tags.get(),
+                                self._blog_path.get())
+
+            showinfo('提示', '发布完成！')
+
+    def _update_category(self):
+        if not self._pyposter:
+            self._init_pyposter()
+
+        if self._pyposter:
+            self._categories.delete(0, END)
+            for name in self._pyposter.get_category_names():
+                self._categories.insert('end', name)
+
+    def _init_pyposter(self):
+        if not all([self._rpc_addr.get(),
+                    self._username.get(),
+                    self._password.get()]):
+            logging.error('Invalid parameters for PyPoster')
+            showerror('错误！', '服务器信息填写不完整！')
+        else:
+            self._pyposter = PyPoster(self._rpc_addr.get(),
+                                      self._username.get(),
+                                      self._password.get())
 
 
 def main():
     config_logger()
     app = Tk()
-    app.title('PyPoster, a portable tool for posting your blog!')
+    app.title('PyPoster，博客发布小工具')
     app.resizable(width=False, height=False)
     logging.info('Start pyposter...')
     MainWindow(app)
